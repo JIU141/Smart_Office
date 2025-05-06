5bbfc821f01d83c7cffafa876ce7f750bf8277d9
JIU141 <1219932710@qq.com>
2025-05-06 20:18:10 +0800

    Initial commit

---
+++ src/components/layout/appaside.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import {isCollapse} from '@/components/layout/isCollapse.ts'
+</script>
+
+<template>
+  <el-aside>
+    <el-scrollbar>
+    <el-menu router unique-opened :collapse="isCollapse">
+      <a href="/" class="logo">
+        <img src="@/assets/logo.svg">
+        <h1>小白办公</h1>
+      </a>
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon> <IEpSetting /> </el-icon><span>xxx</span>
+        </template>
+        <el-menu-item index="/about">
+          <el-icon> <IEpSetting /></el-icon><span>AAA</span>
+        </el-menu-item>
+        <el-menu-item index="/">
+          <el-icon> <IEpSetting /></el-icon><span>BBB</span>
+        </el-menu-item>
+      </el-sub-menu>
+
+      <el-menu-item index="/">
+        <el-icon> <IEpSetting /></el-icon><span>xxx</span>
+      </el-menu-item>
+      <el-menu-item index="/">
+        <el-icon> <IEpSetting /></el-icon><span>xxx</span>
+      </el-menu-item>
+      <el-menu-item index="/">
+        <el-icon> <IEpSetting /></el-icon><span>xxx</span>
+      </el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>
+          <el-icon> <IEpSetting /> </el-icon><span>xxx</span>
+        </template>
+        <el-menu-item index="/">
+          <el-icon> <IEpSetting /></el-icon><span>AAA</span>
+        </el-menu-item>
+        <el-menu-item index="/">
+          <el-icon> <IEpSetting /></el-icon><span>BBB</span>
+        </el-menu-item>
+      </el-sub-menu>
+    </el-menu>
+    </el-scrollbar>
+  </el-aside>
+</template>
+
+<style lang="scss" scoped>
+.logo{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  text-decoration: none;
+  color: black;
+  img{
+    width: 32px;
+    height: 32px;
+  }
+}
+.el-aside {
+  background-color: rgb(232.8, 233.4, 234.6);
+  height: 100vh;
+  width: auto;
+}
+.el-menu{
+  background-color: rgb(232.8, 233.4, 234.6);
+  border-right: none;
+  width: 200px;
+  &.el-menu--collapse{
+    width: 60px;
+    & h1{
+      display: none;
+    }
+  }
+}
+</style>
---
+++ src/components/layout/applayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import appaside from './appaside.vue';
+import appheader from './appheader.vue';
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <appaside />
+      <el-container class="header-and-main">
+        <appheader />
+        <el-main>
+          <el-scrollbar>
+            <RouterView/>
+          </el-scrollbar>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header-and-main{
+  flex-direction: column;
+  height: 100vh;
+}
+.el-main{
+  background-color: rgb(243.9, 244.2, 244.8);
+  padding-right: 0;
+}
+</style>
